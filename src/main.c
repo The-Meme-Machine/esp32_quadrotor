e3eaf0b4d6b5e481129b54f3e2eb3a768acd2157
Stoyanov <djrfung@outlook.com>
2025-06-24 23:56:23 -0700

    Fixed DSHOT implementation

--- src/main.c
+++ src/main.c
@@ -1,31 +1,6 @@
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <esp_timer.h>
-#include <esp_log.h>
-#include <dshot.h>
-#include <driver/gpio.h>
-
-// #define NUM_MOTORS 4
-#define TELEMETRY false // toggle telemetry to false
-
-#define LOG_LOCAL_LEVEL ESP_LOG_INFO
-
-gpio_num_t motor_pins[NUM_MOTORS] = {GPIO_NUM_1, GPIO_NUM_2, GPIO_NUM_3, GPIO_NUM_4};
-
-uint16_t throttles[NUM_MOTORS] = {DSHOT_THROTTLE_MIN, DSHOT_THROTTLE_MIN, DSHOT_THROTTLE_MIN, DSHOT_THROTTLE_MIN};
+#include <main.h>
 
 static const char *TAG = "MAIN";
-
-void command_motors(uint16_t throttle)
-{
-    throttles[0] = throttle;
-    // throttles[1] = throttle;
-    // throttles[2] = throttle;
-    // throttles[3] = throttle;
-
-    send_dshot_frame(&throttles, TELEMETRY);
-    // vTaskDelay(pdMS_TO_TICKS(delay));
-}
 
 // Run the main control loop every 605us (~1.66kHZ)
 void IRAM_ATTR control_loop()
@@ -36,29 +11,48 @@ void IRAM_ATTR control_loop()
 
     if (loop_count < 1250)
     {
-        command_motors(0);
+        throttles[0] = 0;
+        throttles[1] = 0;
+        throttles[2] = 0;
+        throttles[3] = 0;
     }
     else if (loop_count >= 1250 && loop_count < 1500)
     {
-        command_motors(100);
+        throttles[0] = 100;
+        throttles[1] = 100;
+        throttles[2] = 100;
+        throttles[3] = 100;
     }
     else if (loop_count >= 1500 && loop_count < 1900)
     {
-        command_motors(200);
+        throttles[0] = 200;
+        throttles[1] = 200;
+        throttles[2] = 200;
+        throttles[3] = 200;
     }
     else if (loop_count >= 1900 && loop_count < 2300)
     {
-        command_motors(300);
+        throttles[0] = 300;
+        throttles[1] = 300;
+        throttles[2] = 300;
+        throttles[3] = 300;
     }
     else if (loop_count >= 2300 && loop_count < 2700)
     {
-        command_motors(500);
+        throttles[0] = 400;
+        throttles[1] = 400;
+        throttles[2] = 400;
+        throttles[3] = 400;
     }
     else
     {
-        command_motors(550);
+        throttles[0] = 200;
+        throttles[1] = 200;
+        throttles[2] = 200;
+        throttles[3] = 200;
     }
 
+    send_dshot_frame(&throttles, TELEMETRY);
     loop_count++;
 
     uint64_t end = esp_timer_get_time();
@@ -70,16 +64,16 @@ void app_main()
 {
     setup_rmt_channels(motor_pins);
 
-    // throttles[0] = throttle;
+    throttles[0] = 0;
     throttles[1] = 0;
     throttles[2] = 0;
     throttles[3] = 0;
 
     // Do nothing for 3sec
     ESP_LOGI(TAG, "Program is running...");
-    ESP_LOGI(TAG, "Waiting for 3 seconds.");
-    vTaskDelay(pdMS_TO_TICKS(3000));
 
+    // Call control loop on a timer
+    // In the future, call control loop on I2C interrupt
     const esp_timer_create_args_t timer_args = {
         .callback = &control_loop,
         .name = "Main Control Loop",
